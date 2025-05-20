b79cef8145f93dff90df75fb294a1319c838d23b
ITC-c24016 <c24016@std.it-college.ac.jp>
2025-05-20 14:53:51 +0900

    P48 第3章 チャレンジ問題終了

---
+++ Chapter03/challenge3-03.py
@@ -0,0 +1,7 @@
+i = 25
+if i < 10:
+    print("iは10以下です")
+elif i >= 10 and i < 25:
+    print("iは10以上25未満です")
+elif i >= 25:
+    print("iは25以上です")
---
+++ Chapter03/challenge3-06.py
@@ -0,0 +1,9 @@
+age = 25
+
+if age < 5:
+    print("ageは5以下です")
+elif age >= 5 and age < 15:
+    print("ageは5以上15未満です")
+elif age >= 25:
+    print("ageは25以上です")
+
